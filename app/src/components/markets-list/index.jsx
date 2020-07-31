fef6f18e0cb354923a5e3fee78f7f48c1a000513
luzzif <fedeluzzi00@gmail.com>
2020-07-31 23:17:39 +0200

    chore: minor ui fix

--- app/src/components/markets-list/index.jsx
+++ app/src/components/markets-list/index.jsx
@@ -6,7 +6,7 @@ import { Warning } from "../warning";
 export const MarketsList = ({ markets, onMarketClick }) => {
     return (
         <Flex flexDirection="column">
-            <Box mb="24px">
+            <Box mb="20px" mt="20px" px={3}>
                 <Warning />
             </Box>
             <Flex flexWrap="wrap" width="100%">
@@ -15,7 +15,7 @@ export const MarketsList = ({ markets, onMarketClick }) => {
                         <Box
                             width={[1, 1 / 2, 1 / 3]}
                             key={market.conditionId}
-                            p="16px"
+                            p={3}
                         >
                             <MarketCard {...market} onClick={onMarketClick} />
                         </Box>
